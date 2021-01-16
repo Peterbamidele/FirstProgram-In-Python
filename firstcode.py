0eea296a1e4984cffef3ebe745c8af5fbe308627
Peterbamidele <peterbamidele@gmail.com>
2021-01-16 03:09:50 +0100

    first program in python

---
+++ firstcode.py
@@ -0,0 +1,16 @@
+#display line of text using the print() function
+print('Hello, my name is Peter!')
+
+#Printing two line of text by using the new line symbol
+print('This is the first line of text\nHere is another line of text')
+
+#Declearing Variable in python
+
+product_name = 'Delicious Nachos'
+quantity_sold = 33
+unit_price = 12.99
+
+print('We sold ' + str(quantity_sold) + '' + product_name + ' for ' + str(unit_price) + 'each!')
+
+#Using the for fuction to accomplishing resut
+print('we sold {0} {1} for {2} each!'.format(quantity_sold, product_name,unit_price))
